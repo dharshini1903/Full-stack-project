c3bfcf8e08a6f55e5f0135ce39bb182a1ba2d5ab
dharshini1903 <127418268+dharshini1903@users.noreply.github.com>
2023-03-10 13:47:05 +0530

    Add files via upload
    Full stack project

---
+++ FinalProject/controller/exceptionHandling/model/repository/BusManagementRepository.java
@@ -0,0 +1,13 @@
+package com.example.FinalProject.controller.exceptionHandling.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.FinalProject.controller.exceptionHandling.model.BusManagement;
+
+
+
+public interface BusManagementRepository extends JpaRepository<BusManagement, Integer>{
+ 
+
+ 
+}
